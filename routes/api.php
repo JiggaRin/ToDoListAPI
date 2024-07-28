8121ba8cffa0796bee09ff638b9a8490320fdc9a
Oleg <olegl@beedash.com>
2024-07-29 00:22:02 +0300

    set up sanctum auth for project

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\API\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+// Public route to get CSRF cookie
+Route::get('sanctum/csrf-cookie', function () {
+    return response()->json(['message' => 'CSRF cookie set']);
+});
+
+// Auth routes
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
