9b5b53758afbf02ffdae4140a4b7d489968f7b4a
Oleg <olegl@beedash.com>
2024-08-08 16:26:51 +0300

    modify routes, change openAPI documentations and annotations

--- routes/api/task.php
+++ routes/api/task.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('tasks')->middleware('auth:sanctum')->group(function () {
     Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-    Route::post('/store', [TaskController::class, 'store'])->name('tasks.store');
+    Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
 
     Route::middleware('userCheck:App\Models\Task')->group(function () {
-        Route::get('/show/{task}', [TaskController::class, 'show'])->name('tasks.show');
-        Route::put('/change-status/{task}', [TaskController::class, 'changeStatus'])->name('tasks.change-status');
-        Route::put('/update/{task}', [TaskController::class, 'update'])->name('tasks.update');
-        Route::delete('/destroy/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+        Route::get('/{task}', [TaskController::class, 'show'])->name('tasks.show');
+        Route::put('/{task}/status', [TaskController::class, 'changeStatus'])->name('tasks.change-status');
+        Route::put('/{task}', [TaskController::class, 'update'])->name('tasks.update');
+        Route::delete('/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
     });
 });
